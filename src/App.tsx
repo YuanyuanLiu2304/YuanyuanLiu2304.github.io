6dc45f0d53620fc96b2fa4a8a6367406c4c527a7
Yuanyuan Liu <liu00597@algonquinlive.com>
2023-05-03 17:45:21 -0400

    changed homepage

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />}>
+          <Route path="/Home" element={<Home />}>
             Home
           </Route>
           <Route path="/Resume" element={<Resume />}>
